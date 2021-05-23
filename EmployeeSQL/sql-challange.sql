819a4fbd731cf79a257f6c4ea8b83006f0d1c4c7
Veronica O <veronica.o.garcia@gmail.com>
2021-05-23 02:14:26 -0500

    add sql createdb

---
+++ EmployeeSQL/sql-challange.sql
@@ -0,0 +1,40 @@
+CREATE TABLE departments (
+    dept_no VARCHAR,
+    dept_name VARCHAR
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT,
+    dept_no VARCHAR
+);
+
+CREATE TABLE dept_manager (
+
+
+
+);
+
+CREATE TABLE employees (
+    emp_no INT,
+    emp_title VARCHAR,
+    birth_date VARCHAR,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    sex VARCHAR,
+    hire_date date,
+
+);
+
+CREATE TABLE salaries (
+    
+
+
+);
+
+CREATE TABLE titles (
+    
+
+
+);
+
+
